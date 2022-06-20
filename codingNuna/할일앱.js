f6a12675a0b03f4cb781ee5ad7b1a1c9e244afb0
jystwo <jystwo@hanmail.net>
2022-06-20 17:05:19 +0900

    6.20

---
+++ codingNuna/할일앱.js
@@ -0,0 +1,8 @@
+let taskInput = document.getElementById("task-input");
+let addButton = document.getElementById("add-button");
+
+addButton.addEventListener("click", addTask);
+
+function addTask(){
+    
+}
