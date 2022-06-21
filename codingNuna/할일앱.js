25512664b0a0a95d27e41b56eb2c706eee9a52fe
jystwo <jystwo@hanmail.net>
2022-06-21 12:18:35 +0900

    6.21

--- codingNuna/할일앱.js
+++ codingNuna/할일앱.js
@@ -3,10 +3,16 @@ let addButton = document.getElementById("add-button");
 let taskList = [];
 
 addButton.addEventListener("click", addTask);
+taskInput.addEventListener("focus",function(){taskInput.value=""});
 
 function addTask(){
-    let taskContent = taskInput.value;
-    taskList.push(taskContent);
+    let task = {
+        id:randomIDGenerate(),
+        taskContent: taskInput.value,
+        isComplete:false
+    };
+    taskList.push(task);
+    console.log(taskList);
     render();
 }
 
@@ -14,12 +20,20 @@ function render(){
     let resultHTML = '';
     for(let i=0;i<taskList.length;i++){
         resultHTML += `<div class="task">
-        <div>${taskList[i]}</div>
-        <div><button>Check</button>
+        <div>${taskList[i].taskContent}</div>
+        <div><button onclick="toggleComplete('${taskList[i].id}')">Check</button>
         <button>Delete</button>
         </div>
     </div>`;
 }
     
     document.getElementById("task-board").innerHTML = resultHTML;
+}
+
+function toggleComplete(id){
+    console.log("id:",id)
+}
+
+function randomIDGenerate(){
+    return '_' + Math.random().toString(36).substring(2, 9);
 }
