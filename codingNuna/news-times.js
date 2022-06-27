85a68e44e50a486114a14c62eaef2669ef531c2f
jystwo <jystwo@hanmail.net>
2022-06-27 15:42:40 +0900

    6.27

--- codingNuna/news-times.js
+++ codingNuna/news-times.js
@@ -25,9 +25,40 @@ const getLatestNews = async()=>{
     let response = await fetch(url,{headers:header});
     let data = await response.json();
     news = data.articles
-    console.log(news)
+    console.log(news);
+
+    render()
     
 };
+
+const render = ()=>{
+    let newsHTML = '';
+    newsHTML = news.map(item=>{
+        return `<div class="row">
+        <div class="col-lg-4">
+            <img  class="news-image-size" src="${item.media || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU"}">
+        </div>
+        <div class="col-lg-8">
+            <h2>${item.title}</h2>
+            <p>
+                ${item.summary == null || item.summary == ""
+                    ? "내용없음"
+                    : item.summary.length > 200
+                    ? item.summary.substring(0,200)+"..."
+                    : item.summary}
+            </p>
+            <div>
+                ${item.rights || "no source"}
+                ${moment(item.published_date).fromNow()}
+            </div>
+        </div>
+    </div>`
+    }).join('');
+
+
+
+    document.getElementById("news-board").innerHTML=newsHTML
+}
 
 getLatestNews();
 
